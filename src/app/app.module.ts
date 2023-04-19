f914e3db204318ecb5d55b66402d3b05a67d98ef
Joanna Zawalich <joanna.ewa.zawalich@gmail.com>
2023-04-19 22:34:40 +0200

    - Contact module - PageNotFoundComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,11 +12,14 @@ import { AppComponent } from './app.component';
 import { CoreModule} from "./core/core.module";
 import { AboutModule } from "./modules/about/about.module";
 import { HomeModule } from "./modules/home/home.module";
+import { ContactModule } from "./modules/contact/contact.module";
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
@@ -27,6 +30,7 @@ import { HomeModule } from "./modules/home/home.module";
     CoreModule,
     AboutModule,
     HomeModule,
+    ContactModule,
     AppRoutingModule // it should be the last imported module (because there are routing files in other modules)
   ],
   providers: [],
