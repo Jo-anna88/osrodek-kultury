b4cea92f71a58be8ae5b7cfcb0d28a0a9b3c28ef
Joanna Zawalich <joanna.ewa.zawalich@gmail.com>
2023-05-19 16:39:43 +0200

    contact page - contact info and form

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {CONTACT} from "../../modules/mocks/mock-contact";
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-
+    contactInfo = CONTACT;
 }
---
+++ src/app/modules/mocks/mock-contact.ts
@@ -0,0 +1,7 @@
+export const CONTACT = {
+  name: 'CULTURAL CENTRE WIELISZEW',
+  address: 'ul. Kulturalna 1, 05-135 Wieliszew',
+  phone: ' +48 100 200 300',
+  mail: ' biuro@ccw.com'
+}
+export const googleMapUrl : string = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2432.651644817334!2d20.940973076495624!3d52.43110937203796!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471eb8f719c4a1b3%3A0xf759e9a701bbbafd!2sO%C5%9Brodek%20Kultury%20W%20Wieliszewie!5e0!3m2!1spl!2spl!4v1684418197056!5m2!1spl!2spl';
