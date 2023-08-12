85a7fafd2e4deee1e16926c19f8bc5bf59258612
Joanna Zawalich <joanna.ewa.zawalich@gmail.com>
2023-08-12 07:38:35 +0200

    classes' / courses' service

---
+++ src/app/modules/classes/classes.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable, of} from "rxjs";
+import {ICourse} from "./classes/course";
+import {mockCourses} from "./classes/mock-courses";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClassesService {
+
+  private apiUrl = "http://localhost:4200/classes";
+
+  constructor(private http: HttpClient) { }
+
+  //TODO:return max 10 events for page and max 30 events from all of them (pagination needed!)
+  getCourses() : Observable<Array<ICourse>>{
+    //return this.http.get(`${this.apiUrl}`, responseType: );
+    return of(mockCourses);
+  }
+}
