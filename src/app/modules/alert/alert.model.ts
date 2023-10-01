8fef593f4036c0ed99400541832fd9dc33b32d68
Joanna Zawalich <joanna.ewa.zawalich@gmail.com>
2023-10-01 08:30:40 +0200

    alert template and component
    why <img> and <i> are under alerts?

--- src/app/modules/alert/alert.model.ts
+++ src/app/modules/alert/alert.model.ts
@@ -1,13 +1,18 @@
-export interface IAlert {
+export class Alert {
   id?: number;
   severity: Severity;
   description: string;
+  constructor(severity: Severity, description: string) {
+    this.severity = severity;
+    this.description = description;
+    this.id = 1; //todo: backend
+  }
 }
 
 export enum Severity {
-  Success = 'Success',
-  Info = 'Info',
-  Warn = 'Warn',
-  Error = 'Error',
-  None = 'None'
+  Success = 'SUCCESS',
+  Info = 'INFO',
+  Warn = 'WARNING',
+  Error = 'ERROR',
+  None = ''
 }
--- src/app/modules/alert/alert/alert.component.ts
+++ src/app/modules/alert/alert/alert.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Alert, Severity} from "../alert.model";
 
 @Component({
   selector: 'app-alert',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./alert.component.scss']
 })
 export class AlertComponent {
-
+  alert: Alert = new Alert(Severity.Warn, "this is an alert's description");
+  alertIcon: string;
+  errorTypeToIconMapping = new Map<string, string>([
+    [Severity.Success, ''],
+    [Severity.Info, ''],
+    [Severity.Warn,'bi bi-phone'],
+    [Severity.Success, ''],
+    [Severity.None, '']
+  ])
+  constructor() {
+    this.alertIcon = this.errorTypeToIconMapping.get(this.alert.severity)!;
+  }
 }
