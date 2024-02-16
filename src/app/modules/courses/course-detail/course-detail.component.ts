d9fedb672b49accc75e2e2f78e0dd18c477f1457
Joanna Zawalich <joanna.ewa.zawalich@gmail.com>
2024-02-16 14:09:36 +0100

    CRUD operation on course details added

--- src/app/modules/courses/course-detail/course-detail.component.ts
+++ src/app/modules/courses/course-detail/course-detail.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Course, CourseDetails} from "../course";
-import {Observable, Subscription, switchMap} from "rxjs";
+import {Observable, Subscription, switchMap, take} from "rxjs";
 import {ActivatedRoute, ParamMap, Router} from "@angular/router";
 import {CoursesService} from "../courses.service";
 import {ModalService} from "../../../core/services/modal.service";
@@ -15,87 +15,90 @@ import {ModalType} from "../../../shared/components/modal/modal";
 export class CourseDetailComponent implements OnInit {
   course$! : Observable<Course>; // the exclamation mark acts as a non-null assertion operator
   courseDetails : CourseDetails | null | undefined = undefined;
+  id: string = "-1";
+  protected readonly Object = Object;
   //isLoading: boolean = false;
   constructor(
     private route: ActivatedRoute,
-    private router: Router,
     private coursesService: CoursesService,
     private modalService: ModalService
   ) {}
   ngOnInit() {
+    this.id = this.route.snapshot.paramMap.get('id')!;
+    this.course$ = this.coursesService.getCourseById(this.id);
     this.loadData();
   }
   loadData() {
     //this.isLoading = true;
-    this.course$ = this.route.paramMap.pipe(
-      switchMap((params: ParamMap) =>
-          //this.coursesService.getCourseById(+params.get('id')!)) // converting a string to a number by using the ‘+’ unary operator.
-          //(if params.get('id'))
-          this.coursesService.getCourseById(params.get('id')!)
-      )
-    );
-
-    this.route.paramMap.pipe(
-      switchMap((params: ParamMap) =>
-        this.coursesService.getCourseDetailsById(params.get('id')!)
-      )
-    ).subscribe({
+    this.coursesService.getCourseDetailsById(this.id)
+      .subscribe({
         next: (value: CourseDetails) => {
           this.courseDetails = Object.keys(value).length !== 0 ? value : null;
         }
-      }
-    );
+      })
   }
-
-  protected readonly Object = Object;
 
   addDetails() {
     this.modalService.setConfiguration({title: "Add Course Details"});
-    // this.router.navigate([{outlets: {modalOutlet: ['modal', 'delete']}}]);
-    // this.modalService.getEvent().subscribe({
-    //     next: ({user: newUser, password: pswd}) => {
-    //       this.authService.signUp(newUser, pswd)
-    //         .subscribe({
-    //           next: (nUser) => {
-    //             console.log("Response after signup: ", nUser);
-    //           }
-    //         })
-    //       this.modalService.close();
-    //     }
-    //   })
-    // )
-  }
-  updateDetails(courseDetails: CourseDetails) {
-    this.modalService.setConfiguration({title: "Update Course Details", data: courseDetails});
-    this.modalService.openModal(ModalType.UPDATE_COURSE_DETAILS);
-    this.modalService.getModalEvent().subscribe({
-      next: (courseDetails: CourseDetails) => {
-        console.log(courseDetails);
-        this.coursesService.updateCourseDetails(courseDetails)
-          .subscribe({
-            next: (updatedCourseDetails) => {
-              console.log("Response after signup: ", updatedCourseDetails);
-            }
-          });
-        this.modalService.closeModal();
-        this.loadData();
-      }
-    })
-  }
-  deleteDetails(id: string) {
-    this.modalService.setConfiguration({title:"Delete Confirmation", data: "details"});
-    this.modalService.openModal(ModalType.DELETE_CONFIRMATION);
-    this.modalService.getModalEvent().subscribe({
-      next: (isConfirmed: boolean) => {
-        if(isConfirmed) {
-          this.coursesService.deleteCourseDetails(id)
+    this.modalService.openModal(ModalType.CREATE_COURSE_DETAILS);
+    this.modalService.getModalEvent()
+      .pipe(take(1))
+      .subscribe({
+        next: (courseDetails: CourseDetails) => {
+          courseDetails.id = this.id; //courseId;
+          console.log("Course Details to add: ", courseDetails);
+          this.coursesService.addCourseDetails(courseDetails)
+            //.pipe(takeUntil(this.destroy$))
             .subscribe({
-              next: () => {console.log("Deleted successfully")}
+              next: (newCourseDetails) => {
+                console.log("Response: ", newCourseDetails);
+                this.courseDetails = newCourseDetails;
+              }
+            });
+          //this.subscription.unsubscribe(); // it is not needed because of first() ?? or maybe there should be take(1)?
+          this.modalService.closeModal();
+        }
+      });
+  }
+  updateDetails() {
+    this.modalService.setConfiguration({title: "Update Course Details", data: this.courseDetails});
+    this.modalService.openModal(ModalType.UPDATE_COURSE_DETAILS);
+    this.modalService.getModalEvent()
+      .pipe(take(1))
+      .subscribe({
+        next: (courseDetails: CourseDetails) => {
+          console.log("Course Details to update: ", courseDetails);
+          this.coursesService.updateCourseDetails(courseDetails)
+            //.pipe(takeUntil(this.destroy$))
+            .subscribe({
+              next: (updatedCourseDetails) => {
+                console.log("Response after signup: ", updatedCourseDetails);
+                this.courseDetails = updatedCourseDetails;
+              }
             });
           this.modalService.closeModal();
-          this.loadData();
         }
-      }
-    })
+      })
+  }
+  deleteDetails(id: string) {
+    this.modalService.setConfiguration({title: "Delete Confirmation", data: "details"});
+    this.modalService.openModal(ModalType.DELETE_CONFIRMATION);
+    this.modalService.getModalEvent()
+      .pipe(take(1))
+      .subscribe({
+        next: (isConfirmed: boolean) => {
+          if (isConfirmed) {
+            this.coursesService.deleteCourseDetails(id)
+              //.pipe(takeUntil(this.destroy$))
+              .subscribe({
+                next: () => {
+                  console.log("Deleted successfully");
+                  this.courseDetails = null;
+                }
+              });
+            this.modalService.closeModal();
+          }
+        }
+      })
   }
 }
