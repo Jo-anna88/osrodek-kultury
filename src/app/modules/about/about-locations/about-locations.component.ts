1abbab9c45e661683cd9a2392735a15df544bd2e
Joanna Zawalich <joanna.ewa.zawalich@gmail.com>
2024-03-22 05:22:43 +0100

    header-navigation item and about-dropdown item selected/active functionality

---
+++ src/app/modules/about/about-locations/about-locations.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-locations',
+  templateUrl: './about-locations.component.html',
+  styleUrls: ['./about-locations.component.scss']
+})
+export class AboutLocationsComponent {
+
+}
