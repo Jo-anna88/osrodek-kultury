9c4f3bc7869402ef3208476480a38b27321a7b93
Joanna Zawalich <joanna.ewa.zawalich@gmail.com>
2024-04-17 15:21:09 +0200

    - sign-up form validation

--- src/app/core/authorization/sign-up-modal-form/sign-up-modal-form.component.ts
+++ src/app/core/authorization/sign-up-modal-form/sign-up-modal-form.component.ts
@@ -1,8 +1,9 @@
 import {Component} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {ModalService} from "../../services/modal.service";
 import {User, Role} from "../../../shared/models/user.model";
 import {ButtonAction} from "../../../shared/components/modal/modal";
+import {confirmPasswordValidator, LETTERS_ONLY, PASSWORD_REGEX, PHONE_REGEX} from "../../forms/form-validators";
 
 @Component({
   selector: 'app-sign-up-modal-form',
@@ -10,17 +11,43 @@ import {ButtonAction} from "../../../shared/components/modal/modal";
   styleUrls: ['./sign-up-modal-form.component.scss']
 })
 export class SignUpModalFormComponent {
-  signUpForm: FormGroup = this.fb.group({
-    firstName: ['', Validators.required],
-    lastName: ['', Validators.required],
-    phone: [''],
-    email: ['', [Validators.required, Validators.email]],
-    dob: ['', Validators.required],
-    password: ['', [Validators.required]],
-    confirmPassword: ['', [Validators.required]]
-  });
+  signUpForm: FormGroup;
+  passwordControl = new FormControl('', [Validators.required, Validators.pattern(PASSWORD_REGEX)]);
   protected readonly buttonAction = ButtonAction;
-  constructor (private fb: FormBuilder, private modalService: ModalService) {}
+  constructor (private fb: FormBuilder, private modalService: ModalService) {
+    this.signUpForm  = this.fb.group({
+      firstName: ['', [Validators.required, Validators.maxLength(24), Validators.pattern(LETTERS_ONLY)]],
+      lastName: ['', [Validators.required, Validators.maxLength(100), Validators.pattern(LETTERS_ONLY)]],
+      phone: ['', [Validators.required, Validators.maxLength(9), Validators.pattern(PHONE_REGEX)]],
+      dob: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: this.passwordControl,
+      confirmPassword: ['', [Validators.required, confirmPasswordValidator(this.passwordControl)]]
+    });
+  }
+
+  /** FORM CONTROLS GETTERS */
+  get firstName() {
+    return this.signUpForm.get('firstName')!;
+  }
+  get lastName() {
+    return this.signUpForm.get('lastName')!;
+  }
+  get phone() {
+    return this.signUpForm.get('phone')!;
+  }
+  get dob() {
+    return this.signUpForm.get('dob')!;
+  }
+  get email() {
+    return this.signUpForm.get('email')!;
+  }
+  get password() {
+    return this.signUpForm.get('password')!;
+  }
+  get confirmPassword() {
+    return this.signUpForm.get('confirmPassword')!;
+  }
 
   submit() {
     let value = this.signUpForm.value;
@@ -28,12 +55,11 @@ export class SignUpModalFormComponent {
       firstName: value.firstName,
       lastName: value.lastName,
       phone: value.phone,
-      username: value.email,
       dob: value.dob,
-      // headshot
+      username: value.email,
       role: Role.Client
     }
-    let pswd = value.password; // todo: first we need to validate if password is equal to confirmPassword
+    let pswd = value.password;
     let data: {user: User, password: string} = {user: newUser, password: pswd}
     this.modalService.emitModalEvent(data);
   }
--- src/app/core/forms/form-validators.ts
+++ src/app/core/forms/form-validators.ts
@@ -11,9 +11,21 @@ PASSWORD REQUIREMENTS:
 and the special characters @$!%*?&
 */
 export const PASSWORD_REGEX: RegExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+export const PASSWORD_REQUIREMENTS: string = "Password needs to be at least 8 characters long and " +
+  "be a combination of uppercase letters, lowercase letters, numbers, and symbols: @$!%*?& ."
+export const LETTERS_ONLY: RegExp = /^[A-Za-z]+$/;
+export const PHONE_REGEX: RegExp = /^\d{9}$/;
+
 export function passwordValidator(): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null => {
     let validPassword: boolean = PASSWORD_REGEX.test(control.value);
     return !validPassword ? {password: {value: control.value}} : null;
+  }
+}
+
+export function confirmPasswordValidator(passwordControl: AbstractControl): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    let validConfirmPassword: boolean = passwordControl.value === control.value;
+    return !validConfirmPassword ? {confirmPassword: {value: control.value}} : null;
   }
 }
